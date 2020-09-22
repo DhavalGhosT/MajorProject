bc506fe4414b93ebbbe48f43e902fa89ffcd52c5
Amodh Akhelikar <amodh10@gmail.com>
2020-09-22 13:44:52 +0530

    Added webcam video

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import * as faceapi from 'face-api.js';
+
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Participant } from '../models/participant';
 import { LoginService } from '../services/login.service';
+// import { canvas, faceDetectionNet, faceDetectionOptions, saveFile } from './commons';
 
 @Component({
   selector: 'app-room',
@@ -19,9 +22,26 @@ export class RoomComponent implements OnInit {
 
   private roomId;
   private currUser;
+  private mediaoptions;
+  private stream;
+  @ViewChild('video') video: ElementRef;
   participants: Observable<Participant[]>;
 
   ngOnInit(): void {
+    this.mediaoptions = { audio: false,video: true};
+
+    navigator.mediaDevices.getUserMedia(this.mediaoptions)
+    .then(( stream )=>{
+        this.stream = stream;
+    })
+    .catch(( err )=>{console.log(err)});
+    // Promise.all([
+    //   faceapi.nets.tinyFaceDetector.load('/api-model'),
+    //   // faceapi.nets.faceLandmark68Net.loadFromDisk('/api-model'),
+    //   // faceapi.nets.faceRecognitionNet.loadFromDisk('/api-model'),
+    //   // faceapi.nets.faceExpressionNet.loadFromDisk('/api-model')
+    // ])
+
     this.roomId = this.route.snapshot.paramMap.get('id');
     console.log(this.roomId);
     this.loginService
@@ -32,5 +52,13 @@ export class RoomComponent implements OnInit {
       .doc(this.roomId)
       .collection('users')
       .valueChanges();
+  }
+
+  startVideo(){
+    this.video.nativeElement.srcObject = this.stream;
+  }
+
+  stopVideo(){
+    this.video.nativeElement.srcObject = null;
   }
 }
