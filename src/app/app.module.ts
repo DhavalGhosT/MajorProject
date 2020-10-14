fad687dac191b0d15f4a68b620ad474d44a4390f
DhavalGhosT <dhaval.umarale@gmail.com>
2020-10-14 22:14:20 +0530

    Basic changes for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,16 @@ import { RoomComponent } from './room/room.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyB1osOb96GAa3hqPBPOjknQ8rVcWPo0ORI',
+      authDomain: 'majorproject-8b5da.firebaseapp.com',
+      databaseURL: 'https://majorproject-8b5da.firebaseio.com',
+      projectId: 'majorproject-8b5da',
+      storageBucket: 'majorproject-8b5da.appspot.com',
+      messagingSenderId: '426819753096',
+      appId: '1:426819753096:web:b2227e6c20f624fd47bb3d',
+      measurementId: 'G-16YDMYF7TV',
+    }),
     AngularFirestoreModule,
     AngularFireAuthModule,
     FormsModule,
--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,4 +1,4 @@
-import * as faceapi from 'face-api.js';
+// import * as faceapi from 'face-api.js';
 
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
